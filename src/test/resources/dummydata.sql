8bf5fa158706726ba2b130a7f7ce1c7b924af34f
pranav-dixit <pranavsudhir.dixit@sjsu.edu>
2020-05-07 00:52:40 -0700

    Remove errors from sql script

--- src/test/resources/dummydata.sql
+++ src/test/resources/dummydata.sql
@@ -7,12 +7,12 @@ INSERT INTO cartpool.product values (2,"Pepsico","Chips","abc","Lays","5","PIECE
 
 /* Add product store mapping */
 INSERT INTO cartpool.product_store values(1,1);
-INSERT INTO cartpool.product_store values(2,1);
+INSERT INTO cartpool.product_store values(1,2);
 
 /* Add order  */
 /* Needs to update userid(which is the first field) as per current users  */
 INSERT INTO cartpool.orders values(1,NOW(), "PENDING", 1, NULL, 1);
 
 /* Add order product mappings */
-INSERT INTO cartpool.orderdetails values(1,5,1,1);
-INSERT INTO cartpool.orderdetails values(1,6,1,2);
+INSERT INTO cartpool.order_details values(1,5,1,1);
+INSERT INTO cartpool.order_details values(1,6,1,2);
